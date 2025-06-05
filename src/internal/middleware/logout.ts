2c7867c125e2ff949eeca8ecaf56d48e75085aae
Blake Williams <blake.williams@ssoready.com>
2025-06-05 14:14:04 -0700

    Handle logout errors and ensure cookie is correct

--- src/internal/middleware/logout.ts
+++ src/internal/middleware/logout.ts
@@ -4,15 +4,30 @@ import { getConfig } from "../common/config";
 
 export async function logoutMiddleware(req: NextRequest): Promise<NextResponse> {
   const { projectId, vaultDomain } = await getConfig();
-  const accessToken = req.cookies.get(`tesseral_${projectId}_access_token`);
+  const accessToken = req.cookies.get(`tesseral_${projectId}_access_token`)?.value;
 
-  await fetch(`https://${vaultDomain}/api/frontend/v1/logout`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "Cookie": `project_${projectId}_access_token=${accessToken?.value}`,
-    },
-  });
+  // If there is no access token, we can just redirect to the home page.
+  if (accessToken) {
+    const res = await fetch(`https://${vaultDomain}/api/frontend/v1/logout`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Cookie": `tesseral_${projectId}_access_token=${accessToken}`,
+      },
+      body: JSON.stringify({}),
+    });
+
+    if (!res.ok) {
+      return NextResponse.json(
+        {
+          error: await res.text(),
+        },
+        {
+          status: res.status,
+        },
+      );
+    }
+  }
 
   const url = new URL("/", req.url);
   const response = NextResponse.redirect(url.toString());
