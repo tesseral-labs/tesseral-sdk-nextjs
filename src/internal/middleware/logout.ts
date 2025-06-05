c67795c071aeae5a1fb968996dd25d906d86e460
Blake Williams <blakethewilliams@gmail.com>
2025-06-05 16:07:10 -0700

    Unset cookie domain cookies on logout (#8)

--- src/internal/middleware/logout.ts
+++ src/internal/middleware/logout.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { getConfig } from "../common/config";
 
 export async function logoutMiddleware(req: NextRequest): Promise<NextResponse> {
-  const { projectId, vaultDomain } = await getConfig();
+  const { projectId, vaultDomain, cookieDomain } = await getConfig();
   const accessToken = req.cookies.get(`tesseral_${projectId}_access_token`)?.value;
 
   // If there is no access token, we can just redirect to the home page.
@@ -27,6 +27,21 @@ export async function logoutMiddleware(req: NextRequest): Promise<NextResponse> 
 
   response.cookies.delete(`tesseral_${projectId}_access_token`);
   response.cookies.delete(`tesseral_${projectId}_refresh_token`);
+
+  if (cookieDomain?.endsWith(req.nextUrl.hostname)) {
+    // Also include any session cookies that might have been set
+    // on the cookie domain. In order to do this, we need to use
+    // the Set-Cookie header directly, as NextResponse.cookies.delete
+    // does not support setting the Domain attribute.
+    response.headers.append(
+      "Set-Cookie",
+      `tesseral_${projectId}_access_token=; Path=/; Domain=${cookieDomain}; Max-Age=0`,
+    );
+    response.headers.append(
+      "Set-Cookie",
+      `tesseral_${projectId}_refresh_token=; Path=/; Domain=${cookieDomain}; Max-Age=0`,
+    );
+  }
 
   return response;
 }
