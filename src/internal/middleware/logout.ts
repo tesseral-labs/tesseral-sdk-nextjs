685ea01aabfcc09a73cc678b27191037f421ab3d
Blake Williams <blake.williams@ssoready.com>
2025-06-05 10:57:16 -0700

    Don't conditionalize cookie deletion

--- src/internal/middleware/logout.ts
+++ src/internal/middleware/logout.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { getConfig } from "../common/config";
 
 export async function logoutMiddleware(req: NextRequest): Promise<NextResponse> {
-  const { projectId, vaultDomain, devMode } = await getConfig();
+  const { projectId, vaultDomain } = await getConfig();
   const accessToken = req.cookies.get(`tesseral_${projectId}_access_token`);
 
   await fetch(`https://${vaultDomain}/api/frontend/v1/logout`, {
@@ -17,10 +17,8 @@ export async function logoutMiddleware(req: NextRequest): Promise<NextResponse> 
   const url = new URL("/", req.url);
   const response = NextResponse.redirect(url.toString());
 
-  if (devMode) {
-    response.cookies.delete(`tesseral_${projectId}_access_token`);
-    response.cookies.delete(`tesseral_${projectId}_refresh_token`);
-  }
+  response.cookies.delete(`tesseral_${projectId}_access_token`);
+  response.cookies.delete(`tesseral_${projectId}_refresh_token`);
 
   return response;
 }
